d8c4ec47ab4586bb933b35582a21460c19f24f95
Jayden Canales <jaydencanales@Jaydens-MacBook-Air.local>
2023-01-26 20:22:04 -0800

    my first commit for lab1

---
+++ lab1.cpp
@@ -0,0 +1,19 @@
+//Jayden Canales
+//lab-1 jan 24
+
+
+#inclde <iostream>
+using namespace std;
+
+int main(void)
+{
+    int a = rand() % 10 + 1;
+    cout << a << endl;
+    cout << "Hello World\n";
+    cout << "Making a change\n";
+
+
+
+
+    return 0;
+}
